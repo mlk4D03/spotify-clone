e7f174220ed6243ee54592f559132a29a2b8e77b
Mika Döffinger <mika.doeffinger@trivadis.com>
2022-04-13 16:43:19 +0200

    spotify clone

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const playlistIdState = atom({
+    key: "playlistIdState",
+    default: "3NgzgRwNQdgFlcmma0J9Jr"
+});
+
+export const playlistState = atom({
+    key: "playlistState",
+    default: null
+});
